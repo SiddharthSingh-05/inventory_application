8c396dcb138b8de88d4e86797504697932544f0f
Siddharth Singh <siddharth05june@gmail.com>
2026-06-17 20:09:21 +0530

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
+  baseURL: "https://inventory-application-74zu.onrender.com",,
   headers: { "Content-Type": "application/json" },
 });
 
