7b743915c9a81cf50f769b6d15fa3f88141b32f1
Siddharth Singh <siddharth05june@gmail.com>
2026-06-17 20:14:30 +0530

    Update index.js

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://inventory-application-74zu.onrender.com",,
+  baseURL: "https://inventory-application-ki2d.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
